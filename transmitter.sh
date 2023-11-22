1dd821b237e3c1b8f109f042e9a5e8ff12e53e72
Emrehan Çetin <62987366+emrehancetin@users.noreply.github.com>
2023-11-22 22:46:18 +0300

    Create transmitter.sh

---
+++ transmitter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ip4=127.0.0.1
+
+gst-launch-1.0 -e zedsrc ! videoconvert ! x264enc tune=zerolatency bitrate=1000 speed-preset=superfast ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=$ip4 port=3333
